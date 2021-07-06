17a72e4d29c9d3a49d85a54f58653320115ab3ed
Alexander Rovang <wolf_vii@yahoo.com>
2021-07-05 22:10:52 -0400

    added assets and images

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import './assets/styles.css';
  
 const Code = () => {
 
@@ -8,21 +9,20 @@ const Code = () => {
           
           <h1>Full-Stack Software Engineer</h1>
           <p>Flatiron, 2021</p>
-          <a className="link res"download="Resume.doc" target="_blank" href="/Resume.doc"> Download Resume</a>
-          {/* <a className="link res" target="_new" href="/Resume.doc">View Resume</a> */}
+          <a className="link res"download="Resume.doc" target="_blank" href="/images/Resume.doc"> Download Resume</a>
 
           <h3 className="link"><a className="external"href="https://dxrovang.github.io/tarot-trove-frontend/draws" target="_blank" rel="noopener noreferrer"><p>the Tarot Trove</p></a>
           </h3>
-          
-         <img className="background logo"src="React-01.png"/>
-         <img className="logo"src="Ruby-01.png"/>
-         <img className="background logo"src="Redux-01.png"/>
-         <img className="logo background"src="CSS-3-01.png"/>
-         <img className="logo background"src="Rails-01.png"/>
-         <img className="logo background"src="HTML-5-01.png"/>
-         <img className="background logo"src="JavaScript-01.png"/>
-         <img className="background logo"src="Postgresql-01.png"/>
-         <img className=" background logo"src="Bootstrap-01.png"/>
+         
+         <img className="background logo"src="/images/React-01.png"/>
+         <img className="logo"src="/images/Ruby-01.png"/>
+         <img className="background logo"src="/images/Redux-01.png"/>
+         <img className="logo background"src="/images/CSS-3-01.png"/>
+         <img className="logo background"src="/images/Rails-01.png"/>
+         <img className="logo background"src="/images/HTML-5-01.png"/>
+         <img className="background logo"src="/images/JavaScript-01.png"/>
+         <img className="background logo"src="/images/Postgresql-01.png"/>
+         <img className=" background logo"src="/images/Bootstrap-01.png"/>
          {/* <img className="logo"src="Logic.png"/> */}
         
        </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
        <div className="body">
           <div className="center">
-          Alexander Rovang<br></br>
+         
           DXRovang@gmail.com
           <p><a className="link external"href="https://www.linkedin.com/in/alexanderrovang/"target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
           <p><a className="link external"href="https://www.instagram.com/x8nder/"target="_blank" rel="noopener noreferrer">Instagram</a></p>

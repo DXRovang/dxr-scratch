fd4b5a670a777b6e440cd43f9ad79fb09e37e309
Alexander Rovang <wolf_vii@yahoo.com>
2021-07-06 12:48:59 -0400

    adds images to cards on code page

--- src/components/Code.js
+++ src/components/Code.js
@@ -10,12 +10,30 @@ const Code = () => {
           <h1>Full-Stack Software Engineer</h1>
           <p>Flatiron, 2021</p>
           <a className="link res"download="Resume.doc" target="_blank" href="/images/Resume.doc"> Download Resume</a>
-
-          <h3 className="link"><a className="external"href="https://dxrovang.github.io/tarot-trove-frontend/draws" target="_blank" rel="noopener noreferrer"><p>the Tarot Trove</p></a>
+          
+         <div className="row">
+          <h3 className="link column2"><a className="external"href="https://dxrovang.github.io/tarot-trove-frontend/draws" target="_blank" rel="noopener noreferrer">
+             <h1>the Tarot Trove</h1>
+             <img className="project" src="/images/tarotb.jpg"/>
+             <p>React & Rails API</p>
+             </a>
           </h3>
+          <h3 className="link column2"><a className="external"href="https://dxrovang.github.io/tarot-trove-frontend/draws" target="_blank" rel="noopener noreferrer">
+             <h1>the Tarot Trove</h1>
+             <img className="project" src="/images/tarotb.jpg"/>
+             <p>React & Rails API</p>
+             </a>
+          </h3>
+          <h3 className="link column2"><a className="external"href="https://dxrovang.github.io/tarot-trove-frontend/draws" target="_blank" rel="noopener noreferrer">
+             <h1>the Tarot Trove</h1>
+             <img className="project" src="/images/tarotb.jpg"/>
+             <p>React & Rails API</p>
+             </a>
+          </h3>
+          </div>
          
          <img className="background logo"src="/images/React-01.png"/>
-         <img className="logo"src="/images/Ruby-01.png"/>
+         <img className="logo background"src="/images/Ruby-01.png"/>
          <img className="background logo"src="/images/Redux-01.png"/>
          <img className="logo background"src="/images/CSS-3-01.png"/>
          <img className="logo background"src="/images/Rails-01.png"/>

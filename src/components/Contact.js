5afe0b39fd351739e0b2ef96f7957b002dde65c4
Alexander Rovang <wolf_vii@yahoo.com>
2021-07-06 13:09:20 -0400

    adds icons for github, instagram, and linkedin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
           <div className="center">
          
           DXRovang@gmail.com
-          <p><a className="link external"href="https://www.linkedin.com/in/alexanderrovang/"target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-          <p><a className="link external"href="https://www.instagram.com/x8nder/"target="_blank" rel="noopener noreferrer">Instagram</a></p>
-          <p><a className="link external"href="https://github.com/DXRovang"target="_blank" rel="noopener noreferrer">GitHub</a></p>
+          <p><a href="https://www.linkedin.com/in/alexanderrovang/"target="_blank" rel="noopener noreferrer"><img className="linkedin"src="./images/LI-In-Bug.png"/></a></p>
+          <p><a href="https://www.instagram.com/x8nder/"target="_blank" rel="noopener noreferrer"><img className="insta"src="./images/instagram.png"/></a></p>
+          <p><a href="https://github.com/DXRovang"target="_blank" rel="noopener noreferrer"><img src="./images/GitHub-Mark-Light-64px.png"/></a></p>
           <p><a className="link external"href="https://dev.to/dxrovang"target="_blank" rel="noopener noreferrer">Dev.to</a></p>
          </div>
        </div>
